0d796b1bc0d9491314c49ef782a851ae10175234
Pieter <pieter1998.verweij@gmail.com>
2017-04-07 11:19:23 +0200

    Added a very basic script
    Simple script to convert all folders in a folder

---
+++ ConvertAll.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#	This is a basic script to run mangamerger for all directories in the same directory ass 	#  #	ConvertAll is run
+#	
+#	How it works:
+#	Run <thisScript> <outputDirectory>
+#
+currentDir=$PWD;
+output=$1;
+
+mkdir -p $output;
+cd $output;
+output=$PWD;
+cd $currentDir;
+
+for d in */; do
+	inputDir="$currentDir/$d";
+    outputFile="$output/${d%/}.pdf";
+    if [ "$inputDir" != "$output/" ]; then
+    	#echo "$inputDir";
+    	#echo "$outputFile";
+	    MangaMerger "$inputDir" "$outputFile";
+	fi
+done
+
