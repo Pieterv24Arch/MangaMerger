4b9926f7b6fe3c33c2492f876cbf2f6c26e67ffc
Pieter <pieter1998.verweij@gmail.com>
2017-04-07 11:34:25 +0200

    Update ConvertAll.sh

--- ConvertAll.sh
+++ ConvertAll.sh
@@ -3,25 +3,22 @@
 #	This is a basic script to run mangamerger for all directories in the same directory ass 	#  #	ConvertAll is run
 #	
 #	How it works:
-#
-#	For this to work make sure MangaMerger is build and is contained in a /bin folder
 #	Run <thisScript> <outputDirectory>
 #
 currentDir=$PWD;
 output=$1;
 
 mkdir -p $output;
-cd $output;
+cd "$output";
 output=$PWD;
-cd $currentDir;
+cd "$currentDir";
 
 for d in */; do
 	inputDir="$currentDir/$d";
     outputFile="$output/${d%/}.pdf";
     if [ "$inputDir" != "$output/" ]; then
-    	#echo "$inputDir";
-    	#echo "$outputFile";
+		echo "Input $inputDir"
+		#echo "Output $outputDir"
 	    MangaMerger "$inputDir" "$outputFile";
 	fi
 done
-
